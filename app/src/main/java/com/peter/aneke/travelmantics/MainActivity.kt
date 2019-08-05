61693fe7a68fea8527c6a4c72338acdb8115e649
petyr47 <anekepeter47@gmail.com>
2019-08-05 16:41:40 +0100

    added firebase db

--- app/src/main/java/com/peter/aneke/travelmantics/MainActivity.kt
+++ app/src/main/java/com/peter/aneke/travelmantics/MainActivity.kt
@@ -2,8 +2,13 @@ package com.peter.aneke.travelmantics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    val firebaseDb: FirebaseDatabase by lazy {FirebaseDatabase.getInstance()}
+    val databaseReference : DatabaseReference by lazy { firebaseDb.reference }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
