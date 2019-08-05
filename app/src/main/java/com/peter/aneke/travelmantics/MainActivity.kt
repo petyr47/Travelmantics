465a573959fc18b8258d230b548ed123c2ffd121
petyr47 <anekepeter47@gmail.com>
2019-08-05 17:28:40 +0100

    set up databinding

--- app/src/main/java/com/peter/aneke/travelmantics/MainActivity.kt
+++ app/src/main/java/com/peter/aneke/travelmantics/MainActivity.kt
@@ -3,17 +3,32 @@ package com.peter.aneke.travelmantics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.peter.aneke.travelmantics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     val firebaseDb: FirebaseDatabase by lazy {FirebaseDatabase.getInstance()}
     val databaseReference : DatabaseReference by lazy { firebaseDb.reference }
+    val viewModel : InputViewModel by lazy {ViewModelProviders.of(this).get(InputViewModel::class.java)}
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
+
+
     }
 
 
